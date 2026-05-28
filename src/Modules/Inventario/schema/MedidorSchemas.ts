b41bfb3bf55563bf11c8f123eac395d811aa0ee9
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2026-05-28 12:22:22 -0600

    medidores

--- src/Modules/Inventario/schema/MedidorSchemas.ts
+++ src/Modules/Inventario/schema/MedidorSchemas.ts
@@ -7,8 +7,8 @@ export const CreateMedidorSchema = z.object({
       invalid_type_error: 'El número del medidor debe ser un número',
     })
     .int('El número del medidor debe ser un número entero')
-    .min(100000, 'El número del medidor debe tener al menos 6 dígitos y no puede empezar con 0')
-    .max(99999999, 'El número del medidor no puede tener más de 8 dígitos'),
+    .min(1, 'El número del medidor debe tener al menos 1 dígito')
+    .max(999999, 'El número del medidor no puede tener más de 6 dígitos'),
 });
 
 export const UpdateEstadoMedidorSchema = z.object({
